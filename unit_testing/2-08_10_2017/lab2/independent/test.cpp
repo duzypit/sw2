8d2ec53cda2f82d6a2f752c2b1683530b1f30bdb
duzypit <piatek.tomasz@gmail.com>
2017-10-10 11:35:25 +0200

    dir cleanup

--- unit_testing/2-08_10_2017/lab2/independent/test.cpp
+++ unit_testing/2-08_10_2017/lab2/independent/test.cpp
@@ -1,28 +1,42 @@
 //https://www.eriksmistad.no/getting-started-with-google-test-on-ubuntu/
 //https://larry-price.com/blog/2013/10/13/installing-gtest-and-gmock-libs-in-ubuntu-13-dot-04/
 //http://www.bogotobogo.com/cplusplus/google_unit_test_gtest.php
-#include <gtest/gtest.h>
-#include <gmock/gmock-matchers.h>
+//#include <gtest/gtest.h>
+
+#include <iostream>
+#include "/home/test/sw2/unit_testing/2-08_10_2017/lab2/gtest/googlemock/include/gmock/gmock.h"
+//#include "/home/test/sw2/unit_testing/2-08_10_2017/lab2/gtest/googletest/include/gtest/gtest.h"
+
 #include "Picture.hpp"
 #include "Rectangle.hpp"
-using namespace testing;
+#include "Triangle.hpp"
+//using namespace testing;
 
-TEST(LabTest, TestOK)
-{
-    Picture myPicture;
-    Rectangle rec1(2,4);
-    myPicture.addShape(&rec1);
-    ASSERT_THAT(myPicture.getTotalArea(), 8);
-}
-TEST(LabTest, TestNOK)
-{
-    Picture myPicture;
-    Rectangle rec1(2,4);
-    myPicture.addShape(&rec1);
-    ASSERT_THAT(myPicture.getTotalArea(), 9);
-}
+// TEST(LabTest, TestOK)
+// {
+//     Picture myPicture;
+//     Rectangle rec1(2,4);
+//     myPicture.addShape(&rec1);
+//     ASSERT_THAT(myPicture.getTotalArea(), 8);
+// }
+// TEST(LabTest, TestNOK)
+// {
+//     Picture myPicture;
+//     Rectangle rec1(2,4);
+//     myPicture.addShape(&rec1);
+//     ASSERT_THAT(myPicture.getTotalArea(), 9);
+// }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(void) {
+
+    Picture myPicture;
+    myPicture.addShape(new Rectangle(2,4));
+    myPicture.addShape(new Rectangle(4,4));
+    myPicture.addShape(new Triangle(2,4));
+    myPicture.addShape(new Triangle(4,4));
+
+    std::cout << "Total area size = " << myPicture.getTotalArea() << std::endl;    
+  //::testing::InitGoogleTest(&argc, argv);
+  //return RUN_ALL_TESTS();
+    return 0;
 }
