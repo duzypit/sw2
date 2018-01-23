8f9c87f89821ba37bcd7cc48216aa3fd09df6149
duzypit <piatek.tomasz@gmail.com>
2018-01-23 15:23:29 +0100

    added ptr_shared

--- adv_cpp/final/done/ptr_shared.cpp
+++ adv_cpp/final/done/ptr_shared.cpp
@@ -10,3 +10,26 @@ operatory * -> == != < <= > >=
 void swap(?, ?)
 funkcja get()
 */
+#include <iostream>
+
+template<typename T>
+class ptr_shared{
+    public:
+    ptr_shared() : {}
+
+
+    //copy constructible
+
+    //copy assignable
+
+
+    private:
+    T _ptr;
+    std::size_t _refCount = 0;
+};
+
+
+int main()
+{
+    ptr_shared<int> t;
+}
